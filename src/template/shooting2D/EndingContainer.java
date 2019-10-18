d9c08fca8e9872f2f2f047d6085405fbe38701df
s1871065 <s1871065@d-13102-026.ku-kinds.konan-u.ac.jp>
2019-10-18 17:35:25 +0900

    競合修正ローカル

--- src/template/shooting2D/EndingContainer.java
+++ src/template/shooting2D/EndingContainer.java
@@ -1,6 +1,6 @@
 package template.shooting2D;
 
-import java.awt.Event;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.GraphicsConfiguration;
 
@@ -8,9 +8,14 @@ import framework.RWT.RWTContainer;
 import framework.RWT.RWTLabel;
 import framework.RWT.RWTVirtualController;
 import framework.RWT.RWTVirtualKey;
+import framework.audio.BGM3D;
+import framework.audio.Sound3D;
 
 public class EndingContainer extends RWTContainer {
 	private TemplateShooting2DMultiStates game;
+
+	private Sound3D startBGM = BGM3D.registerBGM("data\\sound\\start.wav");
+	private Sound3D selectsound = new Sound3D("data\\sound\\select.wav");
 
 	public EndingContainer(TemplateShooting2DMultiStates game) {
 		this.game = game;
@@ -18,17 +23,23 @@ public class EndingContainer extends RWTContainer {
 
 	@Override
 	public void build(GraphicsConfiguration gc) {
+
 		RWTLabel startLabel = new RWTLabel();
-		startLabel.setString("Finish");
-		startLabel.setRelativePosition(0.3f, 0.5f);
-		Font f = new Font("", Font.PLAIN, 60);
+		setBackground(Color.black);
+		startLabel.setString("èI");
+		startLabel.setColor(Color.white);
+		startLabel.setRelativePosition(0f, 0.5f);
+		Font f = new Font("", Font.PLAIN, 40);
 		startLabel.setFont(f);
 		addWidget(startLabel);
+		BGM3D.playBGM(startBGM);
+		repaint();
 	}
 
 	@Override
 	public void keyPressed(RWTVirtualKey key) {
 		if (key.getVirtualKey() == RWTVirtualController.BUTTON_A) {
+			selectsound.play();
 			game.restart();
 		}
 	}

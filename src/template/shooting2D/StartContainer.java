a1a18b5f4bcd1a5f2ec66c782900402c4804ef36
s1871091 <s1871091@d-13102-030.ku-kinds.konan-u.ac.jp>
2019-10-18 15:55:06 +0900

    スタート画面いじった

--- src/template/shooting2D/StartContainer.java
+++ src/template/shooting2D/StartContainer.java
@@ -1,17 +1,19 @@
 package template.shooting2D;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.GraphicsConfiguration;
 
 import framework.RWT.RWTContainer;
-import framework.RWT.RWTImage;
 import framework.RWT.RWTLabel;
 import framework.RWT.RWTVirtualController;
 import framework.RWT.RWTVirtualKey;
+import framework.audio.BGM3D;
+import framework.audio.Sound3D;
 
 public class StartContainer extends RWTContainer {
 	private TemplateShooting2DMultiStates game;
+	private Sound3D startBGM = BGM3D.registerBGM("data//sound//start.wav");
+	private Sound3D selectsound = new Sound3D("data//sound//select.wav");
 
 	public StartContainer(TemplateShooting2DMultiStates game) {
 		this.game = game;
@@ -19,23 +21,19 @@ public class StartContainer extends RWTContainer {
 
 	@Override
 	public void build(GraphicsConfiguration gc) {
-		RWTImage image = new RWTImage("data\\images\\Background_StartScreen.jpg");
-		addWidget(image);
-
 		RWTLabel startLabel = new RWTLabel();
-		startLabel.setString("カキン・ノ・ダズの冒険");
-		startLabel.setColor(Color.white);
-		startLabel.setRelativePosition(0f, 0.5f);
-		Font f = new Font("", Font.PLAIN, 40);
+		startLabel.setString("Start");
+		startLabel.setRelativePosition(0.3f, 0.5f);
+		Font f = new Font("", Font.PLAIN, 60);
 		startLabel.setFont(f);
 		addWidget(startLabel);
-
-		repaint();
+		BGM3D.playBGM(startBGM);
 	}
 
 	@Override
 	public void keyPressed(RWTVirtualKey key) {
 		if (key.getVirtualKey() == RWTVirtualController.BUTTON_A) {
+			selectsound.play();
 			game.play();
 		}
 	}

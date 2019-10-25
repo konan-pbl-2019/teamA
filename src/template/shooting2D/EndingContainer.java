1cc7a3e50e3a9b9cc69653d91853818a44a4e833
s1871065 <s1871065@d-13102-027.ku-kinds.konan-u.ac.jp>
2019-10-25 15:27:35 +0900

    エンディング変更

--- src/template/shooting2D/EndingContainer.java
+++ src/template/shooting2D/EndingContainer.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.GraphicsConfiguration;
 
 import framework.RWT.RWTContainer;
+import framework.RWT.RWTImage;
 import framework.RWT.RWTLabel;
 import framework.RWT.RWTVirtualController;
 import framework.RWT.RWTVirtualKey;
@@ -23,15 +24,42 @@ public class EndingContainer extends RWTContainer {
 
 	@Override
 	public void build(GraphicsConfiguration gc) {
+		RWTImage image = new RWTImage("data\\images\\Background_EndingScreen.png");
+		image.setSize(1000,1000);
+		addWidget(image);
+		
+		RWTLabel end1Label = new RWTLabel();
+		end1Label.setString("終");
+		end1Label.setColor(Color.white);
+		end1Label.setRelativePosition(0.76f, 0.4f);
+		Font f1 = new Font("", Font.PLAIN, 40);
+		end1Label.setFont(f1);
+		addWidget(end1Label);
 
-		RWTLabel startLabel = new RWTLabel();
-		setBackground(Color.black);
-		startLabel.setString("終");
-		startLabel.setColor(Color.white);
-		startLabel.setRelativePosition(0f, 0.5f);
-		Font f = new Font("", Font.PLAIN, 40);
-		startLabel.setFont(f);
-		addWidget(startLabel);
+		RWTLabel end2Label = new RWTLabel();
+		end2Label.setString("制作・著作");
+		end2Label.setColor(Color.white);
+		end2Label.setRelativePosition(0.6f, 0.5f);
+		Font f2 = new Font("", Font.PLAIN, 40);
+		end2Label.setFont(f2);
+		addWidget(end2Label);
+		
+		RWTLabel end3Label = new RWTLabel();
+		end3Label.setString("―――――――――――――――――");
+		end3Label.setColor(Color.white);
+		end3Label.setRelativePosition(0.6f, 0.55f);
+		Font f3 = new Font("", Font.PLAIN, 40);
+		end2Label.setFont(f3);
+		addWidget(end3Label);
+		
+		RWTLabel end4Label = new RWTLabel();
+		end4Label.setString("ＰＢＬ");
+		end4Label.setColor(Color.white);
+		end4Label.setRelativePosition(0.68f, 0.65f);
+		Font f4 = new Font("", Font.PLAIN, 40);
+		end4Label.setFont(f4);
+		addWidget(end4Label);
+		
 		BGM3D.playBGM(startBGM);
 		repaint();
 	}
